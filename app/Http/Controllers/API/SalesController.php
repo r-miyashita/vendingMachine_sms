d1efb7c93782e21a24e268c48ec6d38bbb9f244f
rym0011 <rym.00001111@gmail.com>
2022-11-06 20:07:31 +0900

    add:api
    購入API作成（新規コントローラー追加）

---
+++ app/Http/Controllers/API/SalesController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\Sale;
+use Illuminate\Http\Request;
+
+class SalesController extends Controller
+{
+    /************************************
+     * 購入処理
+     * 購入分の在庫を減らして売上データ作成する。
+     * 
+     * @param $id 商品id
+     * @return 処理結果、ステータスコード,メッセージ
+     */
+    public function purchase($id)
+    {
+        // 戻り値初期化
+        $result = 0;
+        $error_message = "";
+        $requestId = $id;
+
+        // 商品在庫を減算する
+        $product = Product::findOrFail($id);
+        $result = $product->decrementStock();
+        $error_message = "購入処理完了";
+
+        // 減算結果が完了したら、売上データを作成する 
+        if ($result === 0) {
+            $sale = new Sale();
+            $result = $sale->salesRegistration($id);
+
+            // 売上登録できなかった場合
+            if ($result === 1) {
+                $error_message = "購入処理失敗:売上登録失敗";
+            }
+
+        } elseif ($result === 1) { // 商品在庫を減算できなかった場合
+            $error_message = "購入処理失敗:在庫なし";
+
+        } else {
+            $error_message = "購入処理失敗:例外発生により未処理で修了";
+
+        }
+
+        return compact('result', 'error_message', 'requestId', 'product');
+    }
+}
